309d4384799f83a77ab708a1d379e0088e8640cb
Georgi <georgy.keranov@gmail.com>
2022-05-24 17:24:55 +0300

    Add Posts and Post components

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,15 @@
+import Post from './Post/Post';
+
+export default function Posts() {
+  return (
+    <section className="posts">
+      <h2>Posts</h2>
+
+      <Post />
+
+      <Post />
+
+      <Post />
+    </section>
+  )
+}
