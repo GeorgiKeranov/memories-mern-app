bdd7b31da7807514e6e189dcf3892e9dc6fccc29
Georgi <georgy.keranov@gmail.com>
2022-05-24 17:25:35 +0300

    Include the new components inside the App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+
+      <div className="container">
+        <div className="app__columns">
+          <div className="app__main">
+            <Posts />
+          </div>
+
+          <div className="app__sidebar">
+            <Form />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
