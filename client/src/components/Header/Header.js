f3528cd8f56adf6d543ee24a2681ad1fcc5d328e
Georgi <georgy.keranov@gmail.com>
2022-05-24 17:06:51 +0300

    Add header component and styles

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__title">
+          <h2>MEMORIES</h2>
+        </div>
+      </div>
+    </header>
+  );
+}
