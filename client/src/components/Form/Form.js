bed1c31131495f40f0bdce2b370909cb783732b6
Georgi <georgy.keranov@gmail.com>
2022-05-26 18:08:34 +0300

    Add Form component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,5 +1,23 @@
 export default function Form() {
   return (
-    <div>Form</div>
+    <div className="form-create-memory">
+      <h2>Create a Memory</h2>
+
+      <form>
+        <input type="text" name="creator" placeholder="Creator" />
+
+        <input type="text" name="title" placeholder="Title" />
+
+        <textarea name="message" placeholder="Message"/>
+
+        <input type="text" name="tags" placeholder="Tags" />
+
+        <input type="file" name="image" />
+
+        <input type="submit" value="Submit"/>
+      </form>
+
+      <button>Clear</button>
+    </div>
   )
 }
