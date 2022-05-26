f358abdcb3eabb5e59a0ce752711a726105a3a47
Georgi <georgy.keranov@gmail.com>
2022-05-26 21:41:09 +0300

    Change name of property in post schema

--- server/src/models/post.js
+++ server/src/models/post.js
@@ -5,7 +5,7 @@ const postSchema = mongoose.Schema({
     message: String,
     creator: String,
     tags: [String],
-    selectedFile: String,
+    image: String,
     likeCount: {
         type: Number,
         default: 0
