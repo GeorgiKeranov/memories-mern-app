1e7cbb24958b00286af60736c38e96d4ea03a575
Georgi <georgy.keranov@gmail.com>
2022-05-27 20:59:58 +0300

    Add styles to the header and navigation

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,18 +5,24 @@ export default function Header() {
   return (
     <header className="header">
       <div className="container">
-        <div className="header__title">
-          <h2>MEMORIES</h2>
-        </div>
+        <div className="header__columns">
+          <div className="header__title">
+            <h2>MEMORIES</h2>
+          </div>
 
-        <div className="header__navigation">
-          <nav>
-            <ul>
-              <Link to="/">Home</Link>
+          <div className="header__navigation">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
 
-              <Link to="/register">Register</Link>
-            </ul>
-          </nav>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
