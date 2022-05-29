ced33e1f31b92427d46e0cb41b3d568f9b0dfaa7
Georgi <georgy.keranov@gmail.com>
2022-05-29 15:16:04 +0300

    Remove whitespace from posts.js

--- server/src/routers/posts.js
+++ server/src/routers/posts.js
@@ -4,11 +4,8 @@ import { getPosts, savePost, updatePost, removePost } from '../controllers/posts
 const router = Router();
 
 router.get('/', getPosts);
-
 router.post('/', savePost);
-
 router.patch('/:id', updatePost);
-
 router.delete('/:id', removePost);
 
 export default router;
