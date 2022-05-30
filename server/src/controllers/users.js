749c4ef0fe3324a5b413559e3ef9096896f37027
Georgi <georgy.keranov@gmail.com>
2022-05-30 15:27:39 +0300

    Add user login API endpoint

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,10 +1,40 @@
 import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
 
 export const registerUser = async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
+
+    const token = user.generateToken();
+
+    res.status(200).send({user, token});
+  } catch (error) {
+    res.status(400).send({error: error.message});
+  }
+}
+
+export const loginUser = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    res.status(400).send({error: 'Please provide email and password!'});
+  }
+
+  try {
+    const user = await User.findOne({email});
+    
+    if (!user) {
+      return res.status(400).send({error: 'Invalid credentials!'});
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.status(400).send({error: 'Invalid credentials!'});
+    }
 
     const token = user.generateToken();
 
--- server/src/routers/users.js
+++ server/src/routers/users.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { registerUser } from '../controllers/users.js';
+import { loginUser, registerUser } from '../controllers/users.js';
 
 const router = Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 export default router;
