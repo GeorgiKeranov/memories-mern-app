b7a255e35364d7af723be195b780dd07c6e6dc26
Georgi <georgy.keranov@gmail.com>
2022-05-30 20:32:42 +0300

    Add login user API request and reducer

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -1,15 +1,26 @@
 export async function registerUser(userData) {
-  try {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/users/register`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(userData)
-    });
-    
-    return await response.json();
-  } catch (error) {
-    return { error: error.message };
+  return sendAuthRequest('/users/register', userData);
+}
+
+export async function loginUser(userData) {
+  return sendAuthRequest('/users/login', userData);
+}
+
+async function sendAuthRequest(endpoint, userData) {
+  const response = await fetch(process.env.REACT_APP_API_URL + endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  });
+  
+  const responseObj = await response.json();
+  if (responseObj.error) {
+    throw new Error(responseObj.error);
   }
+  
+  const user = {...responseObj.user, token: responseObj.token};
+  
+  return user;
 }
--- client/src/redux/auth.js
+++ client/src/redux/auth.js
@@ -2,6 +2,22 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import * as usersApi from '../api/users';
 
 export const registerUser = createAsyncThunk('auth/register', usersApi.registerUser);
+export const loginUser = createAsyncThunk('auth/login', usersApi.loginUser);
+
+const authPending = state => {
+  state.isLoading = true;
+}
+
+const authFulfilled = (state, action) => {
+  localStorage.setItem('user', JSON.stringify(action.payload))
+  state.user = action.payload;
+  state.errorMessage = state.isLoading = false;
+}
+
+const authRejected = (state, action) => {
+  state.errorMessage = action.error.message;
+  state.isLoading = false;
+}
 
 const authSlice = createSlice({
   name: 'auth',
@@ -18,33 +34,21 @@ const authSlice = createSlice({
     },
     resetErrorMessage: state => {
       state.errorMessage = false;
+    },
+    setErrorMessage: (state, action) => {
+      state.errorMessage = action.payload;
     }
   },
   extraReducers: {
-    [registerUser.pending]: state => {
-      state.isLoading = true;
-    },
-    [registerUser.fulfilled]: (state, action) => {
-      const response = action.payload;
-      
-      if (response.error) {
-        state.errorMessage = response.error;
-        state.isLoading = false; 
-        return;
-      }
-
-      const user = {...response.user, token: response.token};
-      localStorage.setItem('user', JSON.stringify(user));
-      state.user = response;
-      state.errorMessage = state.isLoading = false;
-    },
-    [registerUser.rejected]: (state, action) => {
-      state.errorMessage = action.payload;
-      state.isLoading = false; 
-    }
+    [registerUser.pending]: authPending,
+    [registerUser.fulfilled]: authFulfilled,
+    [registerUser.rejected]: authRejected,
+    [loginUser.pending]: authPending,
+    [loginUser.fulfilled]: authFulfilled,
+    [loginUser.rejected]: authRejected
   }
 });
 
-export const { logoutUser, resetErrorMessage } = authSlice.actions;
+export const { logoutUser, resetErrorMessage, setErrorMessage } = authSlice.actions;
 
 export default authSlice.reducer;
