e3c9d51c1bc61c9bc2928e55dfdbbac953485a78
Georgi <georgy.keranov@gmail.com>
2022-05-30 15:11:33 +0300

    Integrate Header component with authenticated user

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,7 +1,17 @@
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logoutUser } from '../../redux/auth';
 import './Header.css';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const user = useSelector(state => state.auth.user);
+
+  function logout(e) {
+    e.preventDefault();
+    dispatch(logoutUser());
+  }
+
   return (
     <header className="header">
       <div className="container">
@@ -17,13 +27,21 @@ export default function Header() {
                   <Link to="/">Home</Link>
                 </li>
 
-                <li>
-                  <Link to="/register">Register</Link>
-                </li>
+                {user ? (
+                  <li>
+                    <a href="/logout" onClick={logout}>Logout</a>
+                  </li>
+                ) : (
+                  <>
+                    <li>
+                      <Link to="/register">Register</Link>
+                    </li>
 
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
+                    <li>
+                      <Link to="/login">Login</Link>
+                    </li>
+                  </>
+                )}
               </ul>
             </nav>
           </div>
