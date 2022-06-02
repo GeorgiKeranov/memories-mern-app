4d10a55ebf746679c6c3ffe9d42362e2a987e1a7
Georgi <georgy.keranov@gmail.com>
2022-06-03 00:50:01 +0300

    Change posts per page value for '/posts' endpoint

--- server/src/controllers/posts.js
+++ server/src/controllers/posts.js
@@ -1,7 +1,7 @@
 import Post from '../models/post.js';
 
 export const getPosts = async (req, res) => {
-    const postsPerPage = 1;
+    const postsPerPage = 9;
     let page = 1;
     let search = {};
 
