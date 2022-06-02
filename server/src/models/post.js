c5a6df5ba091af213658aad9690de0dcc0e81481
Georgi <georgy.keranov@gmail.com>
2022-06-02 17:31:46 +0300

    Change the way tags are saved into the database

--- server/src/models/post.js
+++ server/src/models/post.js
@@ -19,6 +19,27 @@ const postSchema = mongoose.Schema({
     }
 });
 
+// Before save or update a post convert tags string to an array
+postSchema.pre('save', convertTagsStringToArray);
+postSchema.pre('updateOne', convertTagsStringToArray);
+
+// Make an array of tags from tags string
+function convertTagsStringToArray(next) {
+    const post = this;
+
+    if (post.isModified('tags')) {
+        let tagsArray = post.tags[0].split('#');
+        // Remove whitespace before and after each tag
+        tagsArray = tagsArray.map(tag => tag.trim());
+        // Remove empty tags from the array
+        tagsArray = tagsArray.filter(tag => tag);
+
+        post.tags = tagsArray;
+    }
+    
+    next();
+}
+
 const Post = mongoose.model('Post', postSchema)
 
 export default Post;
