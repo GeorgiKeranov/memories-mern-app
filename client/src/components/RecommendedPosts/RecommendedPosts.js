7f2a98a680f61e78f0fadaff66ae51ba8c68b509
Georgi <georgy.keranov@gmail.com>
2022-06-04 20:45:41 +0300

    Add loader and styles to RecommendedPosts component

--- client/src/components/RecommendedPosts/RecommendedPosts.js
+++ client/src/components/RecommendedPosts/RecommendedPosts.js
@@ -2,13 +2,18 @@ import { useEffect, useState } from 'react';
 import { getRecommendedPosts } from '../../api/posts';
 import './RecommendedPosts.css';
 import Post from '../Post/Post';
+import Loader from '../Loader/Loader';
 
 export default function RecommendedPosts({excludedPostId, tags}) {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
+
     const getPosts = async () => {
       const fetchedPosts = await getRecommendedPosts({excludedPostId, tags});
+      setIsLoading(false);
       setPosts(fetchedPosts);
     };
 
@@ -17,13 +22,19 @@ export default function RecommendedPosts({excludedPostId, tags}) {
 
   const postsJsx = posts.map(post => {
     return (
-      <Post key={post._id} post={post} />
+      <Post key={post._id} post={post} disableButtons={true} />
     );
   })
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <section className="recommended-posts grow-and-fade-in-animation">
-      <h2>Recommended Posts</h2>
+      <div className="recommended__heading">
+        <h2>You may also like these posts:</h2>
+      </div>
 
       <div className="recommended__posts">
         {postsJsx}
