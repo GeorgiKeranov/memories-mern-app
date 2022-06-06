e2877c3b15f4f663cf9c583013405af7dd1cf53a
Georgi <georgy.keranov@gmail.com>
2022-06-06 17:05:11 +0300

    Add remove filters button to SearchForm component

--- client/src/components/SearchForm/SearchForm.js
+++ client/src/components/SearchForm/SearchForm.js
@@ -1,33 +1,60 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilterFields, getPosts } from '../../redux/posts';
 
 export default function SearchForm() {
   const dispatch = useDispatch();
   const filter = useSelector(state => state.posts.filter);
+  const [formData, setFormData] = useState(filter);
 
   function handleChange(event) {
     const element = event.target;
 
-    dispatch(setFilterFields({
-      ...filter,
-      [element.name]: element.value,
-    }));
+    setFormData(prevData => {
+      return {
+        ...prevData,
+        [element.name]: element.value
+      };
+    });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    dispatch(setFilterFields(formData));
+
+    // Set page to the first one because we are changing the filter
+    const page = 1;
+
+    dispatch(getPosts(page));
+  }
+
+  function removeFilters(event) {
+    event.preventDefault();
+
+    const initialFilters = {title: '', tags: '', page: 1};
+
+    setFormData(initialFilters)
+    dispatch(setFilterFields(initialFilters));
+
+    // Set page to the first one because we are changing the filter
+    const page = 1;
     
-    dispatch(getPosts(1));
+    dispatch(getPosts(page));
   }
 
   return (
     <div className="form grow-and-fade-in-animation">
       <form onSubmit={handleSubmit}>
-        <input type="text" name="title" placeholder="Search Memories" onChange={handleChange} value={filter.title}/>
+        <input type="text" name="title" placeholder="Search Memories" onChange={handleChange} value={formData.title}/>
 
-        <input type="text" name="tags" placeholder="Search Tags" onChange={handleChange} value={filter.tags}/>
+        <input type="text" name="tags" placeholder="Search Tags" onChange={handleChange} value={formData.tags}/>
 
         <button className="btn" type="submit">Search</button>
+
+        {(filter.title || filter.tags) &&
+          <button className="btn btn-cancel" onClick={removeFilters}>Remove Filters</button>
+        }
       </form>
     </div>
   )
