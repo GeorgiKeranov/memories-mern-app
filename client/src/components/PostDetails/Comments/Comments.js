9f6137a6be704eca35b3cab8d51c3719f6c26e31
Georgi <georgy.keranov@gmail.com>
2022-06-06 11:33:42 +0300

    Add styles for comments and comment form

--- client/src/components/PostDetails/Comments/Comments.js
+++ client/src/components/PostDetails/Comments/Comments.js
@@ -1,8 +1,12 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import './Comments.css';
 
 export default function Comments({initialComments}) {
+  const user = useSelector(state => state.auth.user);
   const [comments, setComments] = useState(initialComments);
   const [isLoading, setIsLoading] = useState(false);
+  const [comment, setComment] = useState('');
 
   const commentsJsx = comments.map((comment, index) => {
     return (
@@ -20,9 +24,27 @@ export default function Comments({initialComments}) {
     );
   });
 
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
     <div className="comments">
       {commentsJsx}
+
+      {user &&
+        <form className="comment-form" onSubmit={handleSubmit}>
+          <div className="comment__image">
+            <div className="comment__first-letter">{user.firstName[0]}</div>
+          </div>
+
+          <div className="comment__actions">
+            <input type="text" name="comment" placeholder="Type a comment" value={comment} />
+
+            <button className="btn" type="submit">Comment</button>
+          </div>
+        </form>
+      }
     </div>
   )
 }
